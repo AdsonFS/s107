7606d28abd1180835c0dff041f8a47b9e2c732fa
AdsonFS <adson.santos.dev@gmail.com>
2025-04-08 23:03:40 -0300

    fix: inicia postfix para envio de email

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,3 +1,6 @@
 sudo apt-get install -y mailutils postfix
+
+sudo systemctl start postfix
+sudo systemctl enable postfix
+
 echo "Mandando e-mail com mail do linux" | mail -s "a subject" adson.santos.dev@gmail.com
-cat /var/log/mail.log
