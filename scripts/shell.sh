7eec5b284b21ac81fd0fa13d078e80c0abef790e
Caroliny Abreu <caroliny.abreu@healthbit.com.br>
2025-04-08 22:51:22 -0300

    fix: envio de e-mail

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,2 +1,2 @@
 sudo apt-get install -y mailutils postfix
-echo "Mandando e-mail com mail do linux" | mail -v -s "a subject" adson.santos.dev@gmail.com
+echo "Mandando e-mail com mail do linux" | mail -s "a subject" adson.santos.dev@gmail.com
